18df230fa7e97573fcacd3ea1237a841bab2109f
Iven Prillwitz <iven@prillwitz.at>
2019-10-08 19:18:53 +0200

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Iven Prillwitz on 08.10.19.
+//  Copyright © 2019 Iven Prillwitz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
